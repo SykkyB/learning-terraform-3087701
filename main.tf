9936c419db7e0ada4c9c88b248f5747bc5bb5347
SykkyB <81511470+SykkyB@users.noreply.github.com>
2025-11-06 13:35:07 +0400

    fixed to the latest version declaration

--- main.tf
+++ main.tf
@@ -58,22 +58,24 @@ module "blog_alb" {
   subnets         = module.blog_vpc.public_subnets
   security_groups = [module.blog_sg.security_group_id]
 
-   target_groups = [
-    {
-      name_prefix      = "blog-"
-      backend_protocol = "HTTP"
-      backend_port     = 80
-      target_type      = "instance"
-    }
-  ]
 
-  http_tcp_listeners = [
-    {
-      port               = 80
-      protocol           = "HTTP"
-      target_group_index = 0
+  target_groups = {
+    blog = {
+      name_prefix = "blog-"
+      protocol    = "HTTP"   # was backend_protocol
+      port        = 80       # was backend_port
+      target_type = "instance"
     }
-  ]
+  }
+
+
+  listeners = {
+    http = {
+      port     = 80
+      protocol = "HTTP"
+      forward  = { target_group_key = "blog" }  # references the key above
+  }
+
 
   tags = {
     Environment = "Development"
