e51e78f4fd35b80b816a000bbe1ae619ec48177a
SykkyB <81511470+SykkyB@users.noreply.github.com>
2025-11-06 15:08:47 +0400

    fixed target groups

--- main.tf
+++ main.tf
@@ -43,7 +43,7 @@ module "autoscaling" {
   max_size = 3
 
   vpc_zone_identifier = module.blog_vpc.public_subnets
- # target_group_arns   = module.blog_alb.target_group_arns
+  target_group_arns   = [module.blog_alb.target_group["blog"].arn]
   security_groups     = [module.blog_sg.security_group_id]
   
   image_id               = data.aws_ami.app_ami.id
